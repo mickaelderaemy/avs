5c5b910c2bb883ee350f4fcca2252cba120cab56
Mickael de Raëmy <mickaelderaemy@gmail.com>
2015-08-05 15:08:27 +0200

    CSS modifs in tarifs.php

--- tarifs.php
+++ tarifs.php
@@ -37,8 +37,11 @@
 </div><!--Container-->
 <div class="container">
 	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+		<div class="col-md-2 col-md-offset-2">
+			<img class="img-responsive img-prices" src="images/prices.png">
+		</div>
+		<div class="col-md-6">
+			<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 		</div>
 	</div><!--Row-->
 </div><!--Container-->
